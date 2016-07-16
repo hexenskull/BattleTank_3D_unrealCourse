bc7ea321c938b37d98b837c792a2d6417fa48b4a
Hexen <hexenskull@gmail.com>
2016-07-17 00:21:39 +0800

    116 Creating AI Controller class done. Added separate AIController class for enemies tanks

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI controller not posessing a tank!"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI controller posessing %s"), *(ControlledTank->GetName()))
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("AI controller begin play"))
+}
+
+
